938ab805df535afd65020d4ee5fb604111af2fdd
agung <prakosoagung556@gmail.com>
2026-09-22 20:43:37 +0700

    mengubah data style js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function revealMessage() {
     const message = document.getElementById('message');
     const btn = document.getElementById('openBtn');
+    const music = document.getElementById('bgMusic'); // Ambil elemen audio
 
     if (message.classList.contains('show')) {
         message.classList.remove('show');
         btn.innerText = 'Buka Kejutan ✨';
+        music.pause(); // Matikan musik saat pesan ditutup
     } else {
         message.classList.add('show');
         btn.innerText = 'Tutup Pesan ✖';
+        music.play(); // Putar musik saat pesan dibuka
     }
-}
+} 
